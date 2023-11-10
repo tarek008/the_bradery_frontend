3639cf757c9007b0db8185a20619c5f99338c442
tarek008 <tarek.ayadi@esprit.tn>
2023-11-10 13:24:46 +0100

    update_ResponsiveStyle_LoginForm

--- src/components/authentication/login/LoginForm.js
+++ src/components/authentication/login/LoginForm.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../../store/slices/userSlice";
 import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
+import Item from "@mui/material/Grid";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -27,63 +29,76 @@ const LoginForm = () => {
       });
   };
   return (
-    <Box
-      sx={{
-        maxWidth: "400px",
-        display: "flex",
-        flexDirection: "column",
-        margin: "auto",
-        "& .MuiTextField-root": { margin: "10px 0" },
-        paddingTop: "100px",
-        paddingBottom: "227px",
-      }}
-    >
-      <Typography variant="h4" sx={{ textAlign: "center", mb: 4 }}></Typography>
-      <img
-        src="tbd-icon-logo.png"
-        alt="Your Logo"
-        style={{ maxHeight: "80px", margin: "auto" }}
-      />
-      <TextField
-        required
-        id="email-input"
-        label="Email"
-        type="email"
-        autoComplete="current-email"
-        variant="outlined"
-        value={email || ""}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        required
-        id="password-input"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        variant="outlined"
-        value={password || ""}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Typography
-        variant="body1"
-        sx={{ mt: 2, textAlign: "center", color: "red" }}
-      >
-        {message}
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        sx={{ mt: 2 }}
-        onClick={handleLoginClick}
-      >
-        Me Connecter
-      </Button>
-      <Typography variant="body1" sx={{ mt: 2, textAlign: "center" }}>
-        Pas de compte?{" "}
-        <Link href="#" underline="hover">
-          Créer mon compte
-        </Link>
-      </Typography>
+    <Box>
+      <Grid container justify="center" paddingBottom={"100px"} spacing={2}>
+        <Grid item xs={12} md={12} lg={12}>
+          <Item>
+            <Typography variant="h4" sx={{ textAlign: "center", mb: 4 }} />
+            <img
+              src="tbd-icon-logo.png"
+              alt="Your Logo"
+              style={{ maxHeight: "80px", margin: "auto", display: "block" }}
+            />
+          </Item>
+        </Grid>
+        <Grid item xs={1} md={2} lg={4} />
+        <Grid item xs={10} md={8} lg={4}>
+          <Item>
+            <TextField
+              required
+              id="email-input"
+              label="Email"
+              type="email"
+              autoComplete="current-email"
+              variant="outlined"
+              fullWidth
+              value={email || ""}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Item>
+        </Grid>
+        <Grid item xs={1} md={2} lg={4} />
+
+        <Grid item xs={1} md={2} lg={4} />
+        <Grid item xs={10} md={8} lg={4}>
+          <Item>
+            <TextField
+              required
+              id="password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="outlined"
+              fullWidth
+              value={password || ""}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Item>
+        </Grid>
+        <Grid item xs={1} md={2} lg={4} />
+
+        <Grid item xs={12}>
+          <Typography
+            variant="body1"
+            sx={{ mt: 2, textAlign: "center", color: "red" }}
+          >
+            {message}
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              mt: 2,
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+            onClick={handleLoginClick}
+          >
+            Me Connecter
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
